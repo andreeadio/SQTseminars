1ef3ef68f0f775dbf66bf4e53751e258cae1bf28
Andreea Dioclitean <diocliteanandreea21@stud.ase.ro>
2024-04-09 15:28:23 +0300

    update Builder, new example

--- CTS-Builder/src/main/java/ro/ase/cts/builder/Builder_1.java
+++ CTS-Builder/src/main/java/ro/ase/cts/builder/Builder_1.java
@@ -7,10 +7,10 @@ class Car {
     //required attributes
     private String brand;
     private String model;
-    private String color;
-    private int nrDoors;
 
     //+other optional attributes
+    private String color;
+    private int nrDoors;
     private boolean hasGPS;
     private boolean hasSportMode;
 
@@ -76,14 +76,24 @@ class Car {
 
 
         //constructor with required attributes
-        public CarBuilder(String brand, String model, String color, int nrDoors) {
+        public CarBuilder(String brand, String model) {
             this.brand = brand;
             this.model = model;
-            this.color = color;
-            this.nrDoors = nrDoors;
+
         }
 
         //methods for setting the optional attributes
+        public CarBuilder color ( String color)
+        {
+            this.color=color;
+            return this;
+        }
+        public CarBuilder nrDoors(int nrDoors)
+        {
+            this.nrDoors=nrDoors;
+            return this;
+        }
+
         public CarBuilder setGPS(boolean hasGPS) {
             this.hasGPS = hasGPS;
             return this;
@@ -107,8 +117,13 @@ class Car {
 
 class Main_1 {
     public static void main(String[] args) {
-        Car car1 = new Car.CarBuilder("Audi", "S8", "green", 4).setSportMode(true).setGPS(false).build();
-        Car car2 = new Car.CarBuilder("Audi", "A4", "black",2).build();
+        Car car1 = new Car.CarBuilder("Audi", "S8").color("green")
+                .nrDoors(4)
+                .setSportMode(true)
+                .setGPS(false)
+                .build();
+
+        Car car2 = new Car.CarBuilder("Mercedes", "CLE").color("black").build();
 
         System.out.println(car1.toString());
         System.out.println(car2.toString());
--- CTS-Builder/src/main/java/ro/ase/cts/builder/Builder_2.java
+++ CTS-Builder/src/main/java/ro/ase/cts/builder/Builder_2.java
@@ -1,2 +1,161 @@
-package ro.ase.cts.builder;public class Builder_2 {
+package ro.ase.cts.builder;
+
+import com.sun.tools.javac.Main;
+
+class Car_1 {
+
+    //required attributes
+    private String brand;
+    private String model;
+
+    //+other optional attributes
+    private String color;
+    private int nrDoors;
+    private boolean hasGPS;
+    private boolean hasSportMode;
+
+    //PRIVATE constructor with parameter CarBuilder
+    private Car_1(){}
+
+    //define getters
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getNrDoors() {
+        return nrDoors;
+    }
+
+    public boolean isHasGPS() {
+        return hasGPS;
+    }
+
+    public boolean isHasSportMode() {
+        return hasSportMode;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                ", nrDoors=" + nrDoors +
+                ", hasGPS=" + hasGPS +
+                ", hasSportMode=" + hasSportMode +
+                '}';
+    }
+    //nested static class
 }
+interface Builder {
+
+    public void setColor(String color);
+    public void setNrDoors(int nrDoors) ;
+    public void setHasGPS(boolean hasGPS);
+    public void setHasSportMode(boolean hasSportMode);
+    Car_1 getResult();
+}
+class CarBuilder_1 implements Builder{
+    //required attributes
+    private String brand;
+    private String model;
+    private String color;
+    private int nrDoors;
+
+    //+other optional attributes
+    private boolean hasGPS;
+    private boolean hasSportMode;
+
+
+    //constructor with required attributes
+    public CarBuilder_1(String brand, String model) {
+        this.brand = brand;
+        this.model = model;
+
+    }
+
+    //methods for setting the optional attributes
+    public CarBuilder_1 color ( String color)
+    {
+        this.color=color;
+        return this;
+    }
+    public CarBuilder_1 nrDoors(int nrDoors)
+    {
+        this.nrDoors=nrDoors;
+        return this;
+    }
+
+    //build method
+    public Car_1 build(){
+        return new Car_1(this);
+
+    }
+
+
+    @Override
+    public void setColor(String color) {
+
+    }
+
+    @Override
+    public void setNrDoors(int nrDoors) {
+        this.nrDoors=nrDoors;
+
+    }
+
+    @Override
+    public void setHasGPS(boolean hasGPS) {
+        this.hasGPS = hasGPS;
+    }
+
+    @Override
+    public void setHasSportMode(boolean hasSportMode) {
+        this.hasSportMode = hasSportMode;
+
+    }
+
+    @Override
+    public Car_1 getResult() {
+        return new Car_1();
+    }
+}
+ class CarBuildDirector {
+    private CarBuilder_1 builder;
+
+    public CarBuildDirector(CarBuilder_1 builder) {
+        this.builder = builder;
+    }
+
+    public Car_1 construct() {
+        builder.setColor("Red");
+        return builder.getResult();
+    }
+}
+
+class Main_2{
+    public static void main(String[] args) {
+        Car car1 = new Car.CarBuilder("Audi", "S8").color("green")
+                .nrDoors(4)
+                .setSportMode(true)
+                .setGPS(false)
+                .build();
+
+        Car car2 = new Car.CarBuilder("Mercedes", "CLE").color("black").build();
+
+        System.out.println(car1.toString());
+        System.out.println(car2.toString());
+    }
+}
+
+
